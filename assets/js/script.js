18ed53215fd3a6600d79f0a46f1e131e2aa17b75
CardsClaw <joshr1979@gmail.com>
2023-05-09 11:39:01 -0500

    js skeleton for questions answers and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+//the questions
+const questions=[
+    {
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+     question: "What is the question?",
+     answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+    ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},            
+        {text: "This is the answer", correct: false},
+     ] 
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+{
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+    {
+    question: "What is the question?",
+    answers: [
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: true},
+        {text: "This is the answer", correct: false},
+        {text: "This is the answer", correct: false},
+     ]  
+    },
+];
+
+//The Buttons
+const questionEl = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const playButton = document.getElementById("play-button");
+const highScoresButton = document.getElementById("high-scores")
+
